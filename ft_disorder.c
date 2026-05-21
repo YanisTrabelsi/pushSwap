5331aecc99f2152efa660d3606a3bbecae901482
lherpet <lherpet@f0r9s20.paris.42.school>
2026-05-22 00:48:38 +0200

    ft_disorder a test

--- ft_disorder.c
+++ ft_disorder.c
@@ -10,4 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_lstsize(t_list *lst)
+{
+	int	len;
 
+	len = 0;
+	while (lst)
+	{
+		lst = lst -> next;
+		len++;
+	}
+	return (len);
+}
+
+float	ft_disorder(t_list *lst)
+{
+	float	disorder;
+	int		nb_paires;
+	int		base;
+	t_list	*compare;
+
+	if (!lst)
+		return (NULL);
+	disorder = 0;
+	nb_paires = 1;
+	base = lst -> value;
+	while (lst -> next != NULL)
+	{
+		compare = lst -> next;
+		while(compare)
+		{
+			if (base > compare -> value)
+				disorder += 1.0;
+			compare = compare -> next;
+			nb_paires++;
+		}
+		lst = lst -> next;
+	}
+	return (disorder / nb_paires);
+}
