7f9ad17bbfce70ad70169cbe91cbd2dec9367e33
ytrabels <ytrabels@f0r13s2.paris.42.school>
2026-05-23 02:46:33 +0200

    lstwap correction

--- utils_lst2.c
+++ utils_lst2.c
@@ -6,7 +6,7 @@
 /*   By: ytrabels </var/spool/mail/ytrabels>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 00:48:48 by ytrabels          #+#    #+#             */
-/*   Updated: 2026/05/23 02:38:45 by ytrabels         ###   ########.fr       */
+/*   Updated: 2026/05/23 02:45:22 by ytrabels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -19,7 +19,8 @@ void	ft_lstswap(t_list *lst)
 	next_node = lst->next;
 	temp = next_node->next;
 	next_node->next = lst;
-	lst->prev = next_node;
 	lst->next = temp;
-	next_node->prev = NULL;
+	temp = lst->prev;
+	lst->prev = next_node;
+	next_node->prev = temp;
 }
